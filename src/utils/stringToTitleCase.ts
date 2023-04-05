76227f5ea283fdc80f3d44a9061ceaadbaddcbaf
Ken Yamada <yamakenth@gmail.com>
2023-04-05 13:01:51 -0400

    Remove unnecessary log statement

--- src/utils/stringToTitleCase.ts
+++ src/utils/stringToTitleCase.ts
@@ -2,7 +2,6 @@ export default function stringToTitleCase(s: string) {
   let res: string = "";
 
   const words = s.split(" ");
-  console.log(words);
   words.forEach((word) => {
     res = res.concat(word[0].toUpperCase() + word.slice(1)) + " ";
   });
