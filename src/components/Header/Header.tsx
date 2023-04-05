973ae59e5ff437e62e6ff0e03822cc7d34ecb490
Ken Yamada <yamakenth@gmail.com>
2023-04-05 09:41:55 -0400

    Set color of header elements

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,18 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 export default function Header() {
   return (
     <header>
-      <AppBar position="sticky">
+      <AppBar position="sticky" sx={{ bgcolor: "#fff" }}>
         <Toolbar>
           <div style={{ padding: "12px 0" }}>
-            <Typography variant="h5" component="h1">
+            <Typography
+              variant="h5"
+              component="h1"
+              sx={{ color: "text.primary" }}
+            >
               LeetCode Study Guide
             </Typography>
-            <Typography variant="subtitle2">
-              Inspired by NeetCode 150 and Grind 75
+            <Typography variant="subtitle2" sx={{ color: "text.disabled" }}>
+              List created from NeetCode 150 and Grind 75
             </Typography>
           </div>
         </Toolbar>
